007cfb3578c1e5190e00d40d0a3c4827bfa9424f
owenmyers <OwenDaleMyers@gmail.com>
2018-01-06 10:56:20 -0500

    First commit. Just getting the lattice plot going

---
+++ plot_lattices.py
@@ -0,0 +1,46 @@
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+#from matplotlib.gridspec import GridSpec
+from matplotlib.patches import FancyBboxPatch
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
+
+L = 4
+LINE_SIZE = 0.05
+# link length 
+LL = 1.0
+
+def draw_lattice_backround(ax):
+    patches = []
+
+    for i in range(L):
+        loc = (-LL/2.0,i)
+        cur_horz_rect = Rectangle(loc, L, LINE_SIZE, color='k')
+        patches.append(cur_horz_rect) 
+    for i in range(L):
+        loc = (i,-LL/2.0)
+        cur_horz_rect = Rectangle(loc, LINE_SIZE, L, color='k')
+        patches.append(cur_horz_rect) 
+
+
+    collection = PatchCollection(patches)
+
+    ax.add_collection(collection)
+
+
+def main():
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    draw_lattice_backround(ax)
+
+    plt.axis('equal')
+    #plt.axis('off')
+    #plt.tight_layout()
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
