d32821c462902320fa4306146bfee6f8c9e83e1c
owenmyers <owendalemyers@gmail.com>
2020-08-13 16:45:40 -0400

    grid search seems to be working now with quick-run = True

--- tools/ml/grid_search.py
+++ tools/ml/grid_search.py
@@ -24,6 +24,7 @@ def train_test_model(hparams, x_test, x_train):
         use_bias=True
     )(input_obj)
 
+    fm = None
     for i in range(hparams[HP_N_LAYERS] - 1):
         fm = FEATURE_MAP_START + (i + 1) * hparams[HP_FEATURE_MAP_STEP]
         x = Conv2D(
@@ -64,7 +65,7 @@ def train_test_model(hparams, x_test, x_train):
     )
 
     # log_dir='tensorboard_raw/hp_autoencoder/{}_{}'.format(datetime.now(), current_run_id)
-    log_dir = os.path.join('tensorboard_raw', TENSORBOARD_SUB_DIR)
+    log_dir = os.path.join(RUN_LOCATION, 'tensorboard_raw', TENSORBOARD_SUB_DIR)
     autoencoder.fit(
         x_train, x_train,
         epochs=EPOCHS,
@@ -91,7 +92,7 @@ def run(run_dir, hparams, x_test, x_train):
 
 
 def main():
-    with open('pickle_data.pkl', 'rb') as f:
+    with open(DATA, 'rb') as f:
         all_data = pickle.load(f)
 
     all_data = all_data.astype('float32') / 5.0
@@ -101,13 +102,14 @@ def main():
     x_train = np.reshape(x_train, (len(x_train), L * 2, L * 2, 1))
     x_test = np.reshape(x_test, (len(x_test), L * 2, L * 2, 1))
 
-    with tf.summary.create_file_writer(os.path.join('tensorboard_raw', TENSORBOARD_SUB_DIR)).as_default():
+    with tf.summary.create_file_writer(os.path.join(RUN_LOCATION, 'tensorboard_raw', TENSORBOARD_SUB_DIR)).as_default():
         hp.hparams_config(
             hparams=[HP_BATCH_SIZE, HP_N_LAYERS, HP_FEATURE_MAP_STEP, HP_STRIDE_SIZE],
             metrics=[hp.Metric('binary_crossentropy', display_name='Loss')]
         )
 
     c = 0
+    autoencoder = None
     for batch_size in HP_BATCH_SIZE.domain.values:
         for n_layers in HP_N_LAYERS.domain.values:
             for f_map_step in HP_FEATURE_MAP_STEP.domain.values:
@@ -122,9 +124,10 @@ def main():
                     run_name = "run-%d" % c
                     print('--- Starting trial: %s' % run_name)
                     print({h.name: hparams[h] for h in hparams})
-                    autoencoder = run(os.path.join('tensorboard_raw', TENSORBOARD_SUB_DIR, run_name), hparams)
+                    autoencoder = run(os.path.join('tensorboard_raw', TENSORBOARD_SUB_DIR, run_name), hparams, x_test,
+                                      x_train)
 
-    autoencoder.load_weights('hparam_sweep_model_checkpoint.hdf5')
+    autoencoder.load_weights(CHECKPOINT_FILE)
     autoencoder.save('models/best_hparam_autoencoder.h5')
     layers_to_encoded = int(len(autoencoder.layers) / 2)
     print(layers_to_encoded)
@@ -133,46 +136,50 @@ def main():
         inputs=autoencoder.input,
         outputs=layer_activations
     )
-    activation_model.save()
-    activations = activation_model.predict(x_test)
+    activation_model.save(os.path.join(RUN_LOCATION, 'models/best_hparam_autoencoder.h5'))
+    # activations = activation_model.predict(x_test)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run a parameter sweep to find the best autoencoder.')
-    parser.add_argument('--settings', type=str, help='Settings file location', default='./')
+    parser.add_argument('--settings', type=str, help='Settings file location', required=True)
     parser.add_argument('--run-location', type=str, help='Path you want the run to be done at', default='./')
-    parser.add_argument('--data', type=str, help='Pickled data (this should eventually go in the settings file)',
+    parser.add_argument('--data', type=str, help='Abs. path to pickle data (should eventually go in the settings file)',
                         required=True)
     args = parser.parse_args()
 
     # Make sure the required subdirectories are present
-    os.path.exists(args.run_location + 'model_checkpoints')
-    os.path.exists(args.run_location + 'models')
-    os.path.exists(args.run_location + 'settings')
-    os.path.exists(args.run_location + 'tensorboard_raw')
+    assert os.path.exists(args.run_location + 'model_checkpoints')
+    assert os.path.exists(args.run_location + 'models')
+    assert os.path.exists(args.run_location + 'settings')
+    assert os.path.exists(args.run_location + 'tensorboard_raw')
+    assert os.path.exists(args.data)
 
     config = configparser.ConfigParser()
     config.read(args.settings)
 
-    L = config['Settings']['L']
-    FEATURE_MAP_START = config['Settings']['FEATURE_MAP_START']
-    EPOCHS = config['Settings']['EPOCHS']
+    L = int(config['Settings']['L'])
+    FEATURE_MAP_START = int(config['Settings']['FEATURE_MAP_START'])
+    EPOCHS = int(config['Settings']['EPOCHS'])
     # quick run of single param or full param sweep. Use True for testing.
-    QRUN = config['Settings']['QRUN']
+    QUICK_RUN = config['Settings']['QUICK_RUN']
     VERBOSE = config['Settings']['VERBOSE']
     TENSORBOARD_SUB_DIR = config['Settings']['TENSORBOARD_SUB_DIR']
+    CHECKPOINT_FILE = os.path.join(
+        args.run_location,
+        'checkpoint_{}.hdf5'.format(config['Settings']['timestamp'])
+    )
     CHECKPOINTER = ModelCheckpoint(
-        filepath=os.path.join(
-            args.run_location,
-            'checkpoint_{}.hdf5'.format(config['Settings']['timestamp'])
-        ),
+        filepath=CHECKPOINT_FILE,
         monitor='val_loss',
         verbose=1,
         save_best_only=True,
         mode='auto'
     )
+    DATA = args.data
+    RUN_LOCATION = args.run_location
 
-    if not QRUN:
+    if not QUICK_RUN:
         HP_BATCH_SIZE = hp.HParam('batch_size', hp.Discrete([15, 50]))
         HP_N_LAYERS = hp.HParam('n_layers', hp.Discrete([2, 3]))
         HP_FEATURE_MAP_STEP = hp.HParam('feature_map_step', hp.Discrete([2, 8, 16]))
@@ -182,6 +189,6 @@ if __name__ == "__main__":
         HP_N_LAYERS = hp.HParam('n_layers', hp.Discrete([3]))
         HP_FEATURE_MAP_STEP = hp.HParam('feature_map_step', hp.Discrete([16]))
         HP_STRIDE_SIZE = hp.HParam('stride', hp.Discrete([1]))
-        TENSORBOARD_SUB_DIR = 'qrun'
+        TENSORBOARD_SUB_DIR = 'quick_run'
 
     main()
