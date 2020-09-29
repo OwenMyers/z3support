0d4895576f6c861a75ca398a0c7a9389c9a21bd7
owenmyers <owendalemyers@gmail.com>
2020-09-29 17:14:14 -0400

    Added script for easy grid search run

---
+++ scripts/run_grid_search_cnn_autoencoder.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+$(pwd)/pythonenv/bin/python ./tools/ml/grid_search.py \
+  --settings /home/owen/repos/z3support/tools/ml/settings/2020-08-08T16:21:45.693965.conf \
+  --run-location /home/owen/repos/z3support/tools/ml/
